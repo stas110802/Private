e9db81686859ce31d0cee0877e45d61c19e215b2
stas110802 <71429896+stas110802@users.noreply.github.com>
2021-03-07 19:57:00 +0300

    Update BubbleSortList.h

--- LAB 2/BubbleSortList.h
+++ LAB 2/BubbleSortList.h
@@ -3,30 +3,31 @@
 
 void BubbleSortList(List * list) 
 {
-    List * temp = NULL;
+	List * temp = NULL;
     
-    if (list != NULL) // если список не пустой
+	if (list != NULL) // если список не пустой
 	{
-        while (list->pNext != NULL)// идем до тех пора пока p не пустой 
+		while (list->pNext != NULL)// идем до тех пора пока p не пустой 
 		{
-            temp = list->pNext;// temp - это следующий обьект
-            
-            do 
+		    	temp = list->pNext;// temp - это следующий обьект
+
+			do 
 			{
-                if (list->data > temp->data) // если 1 елемент больше 2
+				if (list->data > temp->data) // если 1 елемент больше 2
 				{// меняем их местами
-                    int value = temp->data;
-                    temp->data = list->data;
-                    list->data = value;
-                }
-                
-                temp = temp->pNext;// шаг вперед для temp
-            } while (temp != NULL);
-            
-            list = list->pNext;/* шаг вперед для list,
-			 тоесть следующий do{}while будет начиная со 2 обьекта,
-			  потом с 3 и так далее
+				    int value = temp->data;
+				    temp->data = list->data;
+				    list->data = value;
+				}
+
+				temp = temp->pNext;// шаг вперед для temp
+		    	} while (temp != NULL);
+
+		    	list = list->pNext;
+			/* шаг вперед для list,
+			   тоесть следующий do{}while будет начиная со 2 обьекта,
+			   потом с 3 и так далее
 			 */
-        }
-    }
+		}
+ 	}
 }
